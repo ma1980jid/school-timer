fec74a3f5ca46d7b6e41f3f23c92f6f5909f1324
ma1980jid <ma1980jid.s@gmail.com>
2026-06-25 21:53:15 +0400

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
-window.SCHOOL_TIMER_SUPABASE_URL = "https://kzhxmwejyfsuorcdvujb.supabase.co/rest/v1/";
+window.SCHOOL_TIMER_SUPABASE_URL = "https://kzhxmwejyfsuorcdvujb.supabase.co";
 window.SCHOOL_TIMER_SUPABASE_ANON_KEY = "sb_publishable_SMJPGEWFq0B0nCyMvu9Sbg_kt7N5hd5";
 window.SCHOOL_TIMER_SLUG = "alsheikh-saif";
