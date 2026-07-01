58ff9f594bd1905c819200822dad39ef715c40f9
ma1980jid <ma1980jid.s@gmail.com>
2026-07-01 09:56:44 +0400

    Clean Safari service worker cache only once

--- pwa-register.js
+++ pwa-register.js
@@ -2,13 +2,29 @@
   if (window.__schoolTimerPwaRegisterLoaded) return;
   window.__schoolTimerPwaRegisterLoaded = true;
 
+  const SAFARI_CLEAN_VERSION = 'safari-clean-02';
+
   function isSafari(){
     const ua = navigator.userAgent || '';
     return /Safari/i.test(ua) && !/Chrome|CriOS|Chromium|Android|FxiOS|Firefox|Edg|OPR/i.test(ua);
   }
 
-  async function unregisterForSafari(){
-    if (!('serviceWorker' in navigator)) return;
+  function safariAlreadyCleaned(){
+    try { return localStorage.getItem('school_timer_safari_clean_version') === SAFARI_CLEAN_VERSION; }
+    catch (error) { return false; }
+  }
+
+  function markSafariCleaned(){
+    try { localStorage.setItem('school_timer_safari_clean_version', SAFARI_CLEAN_VERSION); }
+    catch (error) {}
+  }
+
+  async function unregisterForSafariOnce(){
+    if (safariAlreadyCleaned()) return;
+    if (!('serviceWorker' in navigator)) {
+      markSafariCleaned();
+      return;
+    }
     try {
       const regs = await navigator.serviceWorker.getRegistrations();
       await Promise.all(regs.map((reg) => reg.unregister()));
@@ -19,6 +35,7 @@
         await Promise.all(keys.filter((key) => key.indexOf('school-timer') !== -1).map((key) => caches.delete(key)));
       }
     } catch (error) {}
+    markSafariCleaned();
   }
 
   function register(){
@@ -26,7 +43,7 @@
     if (!window.isSecureContext) return;
 
     if (isSafari()) {
-      unregisterForSafari();
+      unregisterForSafariOnce();
       return;
     }
 
